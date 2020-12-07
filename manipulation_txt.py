9a5dadeba6dc66e3977e022a1496130aa4c00769
leo-ventura30 <leonardo.ventura@outlook.com>
2020-12-07 20:37:50 -0300

    sample create file, write and read

---
+++ manipulation_txt.py
@@ -0,0 +1,25 @@
+import os
+
+text = "Python is best language\n"
+text = text + "to security and\n"
+text += "datascience or JS for web"
+
+# name = input("nome do arquivo")
+# dot = "."
+# ext = ['txt', 'csv', 'html', 'excel']
+# name_file = name+".txt" || name_file = name+dot+ext
+# file = open(os.path.join(name_file))
+
+
+# se não existir oa arquivo, ele criara automaticamente
+print(text)
+file = open(os.path.join('test.txt'), "w")
+
+
+file.close()
+
+file = open("./test.txt")
+content_file = file.read()
+file.close()
+
+print(content_file)
